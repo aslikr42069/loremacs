d4fa1f5bdf4eb3333a2d0fc4b571784c2d94d888
aslikr211234 <arturo211234@gmail.com>
2022-01-31 04:08:43 -0600

    Update main.c

--- main.c
+++ main.c
@@ -6,10 +6,17 @@
 
 int main(){
  char *test_origin = "eo wfd";
- char *test_buffer = "hllorl";
- file_table_t* table = initTable(test_origin);
- table = insertEntry(0, table, 0);
- free(table);
+ char *test_buffer = "hllorl!";
+ piece_table_t* table = initTable(test_origin);
+ insertEntry(0, table, 0);
+ insertEntry(2, table, 1);
+ insertEntry(3, table, 2);
+ insertEntry(9, table, 6);
+ char *read_buffer = readTable(table, test_origin, test_buffer);
+ printTable(table);
+ printf("%s\n", read_buffer);
+ freeTable(table);
+ free(read_buffer);
  return 0; 
 }
 
