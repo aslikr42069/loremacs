b38596bf7e3a3f7dc1b2d1e2cb4e1206e8b67885
aslikr42069 <arturo211234@gmail.com>
2022-02-01 21:37:48 -0600

    Small, unimportant updates

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# loremacs
+# Loremacs
 Text editor (not emacs clone, just sounded cool)
+## Building Instructions
+
+For now, all you need is GCC, GNU make, and some implementation of the
+standard C library. Then
+```
+git clone https://github.com/aslikr42069/loremacs.git
+cd loremacs
+make
+```
--- main.c
+++ main.c
@@ -8,10 +8,11 @@ int main(){
  char *test_origin = "eo wfd";
  char *test_buffer = "hllorl!";
  piece_table_t* table = initTable(test_origin);
+ deleteEntry(4, table);
  insertEntry(0, table, 0);
  insertEntry(2, table, 1);
  insertEntry(3, table, 2);
- insertEntry(9, table, 6);
+ insertEntry(8, table, 6);
  char *read_buffer = readTable(table, test_origin, test_buffer);
  printTable(table);
  printf("%s\n", read_buffer);
